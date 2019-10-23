faae80280edf721eead877761cb62890c5e0d1d6
Bogdan Lupu <bogdan.lupu@endava.com>
2019-10-23 10:19:52 +0300

    Refactored into something less gross

--- src/main/java/com/fii/or/simplex/SimplexMain.java
+++ src/main/java/com/fii/or/simplex/SimplexMain.java
@@ -7,23 +7,49 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class SimplexMain {
-    private static double variables;
-    private static double restrictions;
-
     public static void main(String[] args) {
+        System.out.println("First example");
+        new SimplexMain().applySimplex("D:\\Simplex\\src\\main\\java\\com\\fii\\or\\simplex\\homework1.txt");
+        System.out.println("Second example");
+        new SimplexMain().applySimplex("D:\\Simplex\\src\\main\\java\\com\\fii\\or\\simplex\\homework2.txt");
+        System.out.println("Third example");
+        new SimplexMain().applySimplex("D:\\Simplex\\src\\main\\java\\com\\fii\\or\\simplex\\homework3.txt");
+    }
+
+    void applySimplex(String filePath) {
+        List<List<Double>> simplexTable = readLinearProgram(filePath);
+        while (!isDone(simplexTable)) {
+            int pivotColumnIndex = getFirstNegativeIndex(simplexTable);
+            List<Double> ratios = computeRatios(simplexTable, pivotColumnIndex);
+            int pivotRowIndex;
+            try {
+                pivotRowIndex = getSmallestPositiveRatio(ratios);
+            } catch (ArithmeticException e) {
+                System.out.println("Problem is unbounded");
+                return;
+            }
+            applyPivoting(simplexTable, pivotRowIndex, pivotColumnIndex);
+        }
+        if (hasMultipleSolutions(simplexTable)) {
+            System.out.println("Problem has more than one optimal solutions");
+        }
+        getSolution(simplexTable);
+    }
+
+    private List<List<Double>> readLinearProgram(String filePath) {
         List<String> allLines = Lists.newArrayList();
         try {
             allLines = Files.readAllLines(
-                    Paths.get("D:\\Simplex\\src\\main\\java\\com\\fii\\or\\simplex\\test3.txt"));
+                    Paths.get(filePath));
         } catch (Exception e) {
             e.printStackTrace();
             System.exit(1);
         }
-        restrictions = allLines.size() - 1;
-        variables = allLines.get(0).split(" ").length;
+        double variables = allLines.get(0).split(" ").length;
 
         List<List<Double>> restrictions = Lists.newArrayList();
         List<Double> targetFunction = null;
@@ -62,28 +88,10 @@ public class SimplexMain {
         }
         z.add(targetFunction.get((int) variables - 1));
         simplexTable.add(z);
-        System.out.println(simplexTable);
-
-        while (!isDone(simplexTable)) {
-            int pivotColumnIndex = getFirstNegativeIndex(simplexTable);
-            System.out.println(pivotColumnIndex);
-            List<Double> ratios = computeRatios(simplexTable, pivotColumnIndex);
-            int pivotRowIndex;
-            try {
-                pivotRowIndex = getSmallestPositiveRatio(ratios);
-            } catch (ArithmeticException e) {
-                System.out.println("Problem is unbounded");
-                return;
-            }
-            applyPivoting(simplexTable, pivotRowIndex, pivotColumnIndex);
-        }
-        if (hasMultipleSolutions(simplexTable)) {
-            System.out.println("Problem has more than one optimal solutions");
-        }
-        getSolution(simplexTable);
+        return simplexTable;
     }
 
-    private static void getSolution(List<List<Double>> simplexTable) {
+    private void getSolution(List<List<Double>> simplexTable) {
         List<Double> solution = new ArrayList<>();
         while (solution.size() < simplexTable.get(0).size()-1) solution.add(0.0);
         for (int columnIndex = 0; columnIndex < simplexTable.get(0).size() - 1; columnIndex++) {
@@ -105,8 +113,6 @@ public class SimplexMain {
                 }
             }
             if (isBasic && hasOnlyOneOne) {
-                System.out.println(columnIndex);
-                System.out.println(solution.size());
                 solution.set(columnIndex, simplexTable.get(onePosition).get(simplexTable.get(onePosition).size()-1));
             }
         }
@@ -116,24 +122,27 @@ public class SimplexMain {
                 .get(simplexTable.get(simplexTable.size() - 1).size() - 1));
     }
 
-    private static int getSmallestPositiveRatio(List<Double> ratios) throws ArithmeticException {
-        return ratios.indexOf(ratios.subList(0, ratios.size()-1).stream()
-                .mapToDouble(x -> x < 0 ? Double.MAX_VALUE : x)
+    private int getSmallestPositiveRatio(List<Double> ratios) throws ArithmeticException {
+        double minValue = ratios.subList(0, ratios.size()-1)
+                .stream()
+                .filter(Objects::nonNull)
+                .mapToDouble(x -> x)
                 .min()
-                .orElseThrow(ArithmeticException::new));
+                .orElseThrow(ArithmeticException::new);
+        return ratios.indexOf(minValue);
     }
 
-    private static boolean hasMultipleSolutions(List<List<Double>> simplexTable) {
+    private boolean hasMultipleSolutions(List<List<Double>> simplexTable) {
         int numberOfBasicVariables = simplexTable.size();
         long numberOfZeros = simplexTable.get(simplexTable.size()-1).stream().filter(x -> x == 0.0).count();
         return numberOfBasicVariables == numberOfZeros;
     }
 
-    private static void applyPivoting(
+    private void applyPivoting(
             List<List<Double>> simplexTable,
             int pivotRowIndex,
             int pivotColumnIndex) {
-        System.out.println("Pivoting " + pivotRowIndex + " " + pivotColumnIndex);
+//        System.out.println("Pivoting " + pivotRowIndex + " " + pivotColumnIndex);
         double pivotValue = simplexTable.get(pivotRowIndex).get(pivotColumnIndex);
         simplexTable.set(pivotRowIndex, simplexTable.get(pivotRowIndex)
                 .stream()
@@ -153,14 +162,14 @@ public class SimplexMain {
         }
     }
 
-    private static List<Double> computeRatios(List<List<Double>> simplexTable, int pivotIndex) {
+    private List<Double> computeRatios(List<List<Double>> simplexTable, int pivotIndex) {
         return simplexTable
                 .stream()
-                .map(row -> row.get(pivotIndex) > 0 ? row.get(row.size() - 1) / row.get(pivotIndex) : Double.MAX_VALUE)
+                .map(row -> row.get(pivotIndex) > 0 ? row.get(row.size() - 1) / row.get(pivotIndex) : null)
                 .collect(Collectors.toList());
     }
 
-    private static int getFirstNegativeIndex(List<List<Double>> simplexTable) {
+    private int getFirstNegativeIndex(List<List<Double>> simplexTable) {
         double firstNegativeValue = simplexTable.get(simplexTable.size() - 1)
                 .stream()
                 .mapToDouble(x -> x)
@@ -170,7 +179,7 @@ public class SimplexMain {
         return simplexTable.get(simplexTable.size() - 1).indexOf(firstNegativeValue);
     }
 
-    private static boolean isDone(List<List<Double>> simplexTable) {
+    private boolean isDone(List<List<Double>> simplexTable) {
         return simplexTable.get(simplexTable.size() - 1)
                 .stream()
                 .allMatch(x -> x >= 0);
